3847c771c33c328933a681b00b02c7f729eed390
Tomasz Ducin <tomasz@ducin.dev>
2025-02-23 01:19:07 +0100

    lesson-07

--- src/routes/employees.tsx
+++ src/routes/employees.tsx
@@ -3,14 +3,8 @@ import { getEmployees } from '../api/employee.api';
 
 export const Route = createFileRoute('/employees')({
   component: EmployeesComponent,
-  // loader: async () => [
-  //   { id: 1, firstName: 'John', lastName: 'Doe' },
-  //   { id: 2, firstName: 'Jane', lastName: 'Smith' },
-  //   { id: 3, firstName: 'Alice', lastName: 'Johnson' },
-  //   { id: 4, firstName: 'Bob', lastName: 'Brown' },
-  //   { id: 5, firstName: 'Charlie', lastName: 'White' },
-  // ]
-  loader: () => getEmployees()
+  loader: () => getEmployees(),
+  // preload: false
 })
 
 function EmployeesComponent() {
