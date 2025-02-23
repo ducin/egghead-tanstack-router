f078486c918c2086fdd8915608bfb6b4b68dec44
Tomasz Ducin <tomasz@ducin.dev>
2025-02-23 02:24:21 +0100

    lesson-11

--- src/routes/employees/$employeeId.tsx
+++ src/routes/employees/$employeeId.tsx
@@ -1,11 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { getEmployeeById } from '../../api/employee.api'
 
 export const Route = createFileRoute('/employees/$employeeId')({
   component: RouteComponent,
+  loader: ({ params: { employeeId } }) => {
+    const id = Number(employeeId)
+    return getEmployeeById(id)
+  }
 })
 
 function RouteComponent() {
-  return <div>
-    name: John Doe
-  </div>
+  const employeeDetails = Route.useLoaderData()
+  return (
+    <div className="p-2">
+      <h2 className='text-xl'>Employee Details</h2>
+      <p>
+        <strong>ID:</strong> {employeeDetails.id}
+      </p>
+      <p>
+        <strong>Name:</strong> {employeeDetails.name}
+      </p>
+      <p>
+        <strong>Position:</strong> {employeeDetails.position}
+      </p>
+      <p>
+        <strong>Skills:</strong> {employeeDetails.skills.join(', ')}
+      </p>
+    </div>
+  )
 }
